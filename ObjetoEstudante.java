e7805d8ac87c0bf828f268c09e4d05fcafe004a2
Sophia Galvão <159277699+sophiaglv@users.noreply.github.com>
2024-10-09 16:21:12 -0300

    Exemplos

---
+++ ObjetoEstudante.java
@@ -0,0 +1,13 @@
+package encapsulamento;
+
+public class ObjetoEstudante {
+
+	public static void main(String[] args) {
+		Estudante estudante1 = new Estudante("Jabulani", 10, 10);
+		
+		estudante1.calcularMedia();
+		
+		estudante1.exibirInformacoes();
+	}
+
+}
